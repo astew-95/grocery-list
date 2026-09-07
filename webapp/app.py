4686ddeede0543a5029125602ca77ba35ecc3628
will <wevfreeman@example.com>
2026-09-07 16:13:25 -0400

    Created add and delete functionality for shopping lists

--- webapp/app.py
+++ webapp/app.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from fastapi.requests import Request
+from fastapi.responses import RedirectResponse
 from webapp import dependencies
 
 from utils import *
@@ -33,4 +34,30 @@ def store_list(
             "selected_store": store,
             "items": get_expanded_shop_list(store, load_items()),
         }
+    )
+
+@app.post("/{store}/additem")
+def add_to_list(
+    store: str,
+    item: str = Form(""),
+):
+    items = load_items()
+    add_item(store, item, items, show=False)
+    save_items(items)
+    return RedirectResponse(
+        url=f"/{store}",
+        status_code=303,
+    )
+
+@app.post("/{store}/delitem")
+def del_from_list(
+    store: str,
+    item: str = Form(""),
+):
+    items = load_items()
+    remove_items(store, [item], items)
+    save_items(items)
+    return RedirectResponse(
+        url=f"/{store}",
+        status_code=303,
     )
